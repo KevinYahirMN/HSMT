90c01d5b2d6ddafe43154c3dde1ff4ce3fe9c5bf
Jace021099 <84101530+Jace021099@users.noreply.github.com>
2023-03-01 14:23:54 -0800

    Escalas
    Se solucionaron errores en algunas escalas

--- Scales/basicScales/basicScalesFemale.php
+++ Scales/basicScales/basicScalesFemale.php
@@ -1,62 +1,80 @@
 <?php
+class basicScalesFemale{
+    private $c;
 
-function scale_Mf_Female(){
-    $answer[0]f
-    $answer[3]t
-    $answer[18]f
-    $answer[24]t
-    $answer[25]f
-    $answer[26]f
-    $answer[61]t
-    $answer[62]f
-    $answer[63]t
-    $answer[66]t
-    $answer[67]f
-    $answer[68]f
-    $answer[73]t
-    $answer[75]f
-    $answer[79]t
-    $answer[85]f
-    $answer[102]f
-    $answer[103]f
-    $answer[106]f
-    $answer[111]t
-    $answer[118]t
-    $answer[119]f
-    $answer[120]t
-    $answer[121]t
-    $answer[127]t
-    $answer[131]f
-    $answer[132]f
-    $answer[136]t
-    $answer[162]f
-    $answer[165]f
-    $answer[176]t
-    $answer[183]f
-    $answer[186]t
-    $answer[190]t
-    $answer[192]f
-    $answer[193]f
-    $answer[195]t
-    $answer[196]f
-    $answer[198]f
-    $answer[200]f
-    $answer[204]t
-    $answer[206]f
-    $answer[208]f
-    $answer[218]t
-    $answer[230]f
-    $answer[234]f
-    $answer[235]t
-    $answer[236]f
-    $answer[238]f
-    $answer[250]t
-    $answer[253]f
-    $answer[255]t
-    $answer[256]f
-    $answer[267]f
-    $answer[270]t
-    $answer[271]f
- }
+    //sumatoria de respuesta verdadero
+    function calif_True($answer){
+       if($answer == 'true'){
+          $this->c++;
+       }
+    }
+    
+    //Sumatoria de respuesta falso
+    function calif_False($answer){
+      if($answer == 'false'){
+          $this->c++;
+      }
+    }
 
+    function scale_Mf_Female($answer = array()){
+        $this->c = 0;
+        $this->calif_False($answer[0]);
+        $this->calif_True($answer[3]);
+        $this->calif_False($answer[18]);
+        $this->calif_True($answer[24]);
+        $this->calif_False($answer[25]);
+        $this->calif_False($answer[26]);
+        $this->calif_True($answer[61]);
+        $this->calif_False($answer[62]);
+        $this->calif_True($answer[63]);
+        $this->calif_True($answer[66]);
+        $this->calif_False($answer[67]);
+        $this->calif_False($answer[68]);
+        $this->calif_True($answer[73]);
+        $this->calif_False($answer[75]);
+        $this->calif_True($answer[79]);
+        $this->calif_False($answer[85]);
+        $this->calif_False($answer[102]);
+        $this->calif_False($answer[103]);
+        $this->calif_False($answer[106]);
+        $this->calif_True($answer[111]);
+        $this->calif_True($answer[118]);
+        $this->calif_False($answer[119]);
+        $this->calif_True($answer[120]);
+        $this->calif_True($answer[121]);
+        $this->calif_True($answer[127]);
+        $this->calif_False($answer[131]);
+        $this->calif_False($answer[132]);
+        $this->calif_True($answer[136]);
+        $this->calif_False($answer[162]);
+        $this->calif_False($answer[165]);
+        $this->calif_True($answer[176]);
+        $this->calif_False($answer[183]);
+        $this->calif_True($answer[186]);
+        $this->calif_True($answer[190]);
+        $this->calif_False($answer[192]);
+        $this->calif_False($answer[193]);
+        $this->calif_True($answer[195]);
+        $this->calif_False($answer[196]);
+        $this->calif_False($answer[198]);
+        $this->calif_False($answer[200]);
+        $this->calif_True($answer[204]);
+        $this->calif_False($answer[206]);
+        $this->calif_False($answer[208]);
+        $this->calif_True($answer[218]);
+        $this->calif_False($answer[230]);
+        $this->calif_False($answer[234]);
+        $this->calif_True($answer[235]);
+        $this->calif_False($answer[236]);
+        $this->calif_False($answer[238]);
+        $this->calif_True($answer[250]);
+        $this->calif_False($answer[253]);
+        $this->calif_True($answer[255]);
+        $this->calif_False($answer[256]);
+        $this->calif_False($answer[267]);
+        $this->calif_True($answer[270]);
+        $this->calif_False($answer[271]);
+        return $this->c;
+     }
+}
 ?>
