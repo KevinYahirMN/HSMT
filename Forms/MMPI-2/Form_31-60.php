c808ea664f39a9739598b7a1d869d12ba4aa3d50
edgarms17 <99705430+edgarms17@users.noreply.github.com>
2023-03-06 22:05:35 -0800

    31-60 php con diseño

--- Forms/MMPI-2/Form_31-60.php
+++ Forms/MMPI-2/Form_31-60.php
@@ -1,244 +1,324 @@
 <?php
-    session_start();
-    $_SESSION["answer"] = $_POST["Q"];
+session_start();
+$answer = $_SESSION['answer'];
+
+$postanswer = $_POST['Q'];
+
+for($i = 1; $i < 31; $i++){
+   array_push($answer, $postanswer[$i]);
+}
+
+$_SESSION['answer'] = $answer;
 ?>
+
+
+
 <!DOCTYPE html>
-<html lang="es">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title>
-        Examen ejemplo jsjsjs
-    </title>
-    <body>
-        <form method="post" action="../../Pruebas/pruebas.php">
-        <!-- -->
-        <!--<form method="post" action="Form_61-90.php">-->
-            <!--Pregunta 31-->
-            <br><p>Me cuesta bastante concentrarme en una tarea o trabajo</p>
+<html>
+    <link rel="stylesheet" href="style.css">
+   <head>
+    <title>Preguntas 31-60</title>
+    <link rel="shortcut icon" href="ico.ico" />
+   </head>
+   <body>
+    <header>
+        <h1 id="titulo"> Cuestionario </h1>
+        <h2> Responda las siguientes preguntas </h2>
+      </header>
+      
+      <main id="principal">
+        <p id="description"> Lea con atención las siguientes preguntas y responda con total honestidad</p>
+      
+        <form id="formulario">
+          
+        <form method="post" action="Form_61-90.php">
+         <!--Pregunta 31-->
+         <p>Me cuesta bastante concentrarme en una tarea o trabajo</p>
             <input type="radio" value="true" name="Q[1]">
-            <label>verdadero</label>
+            <label>verdadero</label> <br>
             <input type="radio" value="false" name="Q[1]">
             <label>falso</label>
 
             <!--Pregunta 32-->
-            <br><p>He tenvalueo muy peculiares y extrañas experiencias</p>
+            <p>He tenido muy peculiares y extrañas experiencias</p>
             <input type="radio" value="true" name="Q[2]">
-            <label>verdadero</label>
+            <label>verdadero</label><br>
             <input type="radio" value="false" name="Q[2]">
             <label>falso</label>
 
             <!--Pregunta 33-->
-            <br><p>Raras veces me preocupo por mi salud</p>
+            <p>Raras veces me preocupo por mi salud</p>
             <input type="radio" value="true" name="Q[3]">
-            <label>verdadero</label>
+            <label>verdadero</label><br>
             <input type="radio" value="false" name="Q[3]">
             <label>falso</label>
 
             <!--Pregunta 34-->
-            <br><p>Nunca me he visto en dificultades a causa de mi conducta sexual</p>
+            <p>Nunca me he visto en dificultades a causa de mi conducta sexual</p>
             <input type="radio" value="true" name="Q[4]">
-            <label>verdadero</label>
+            <label>verdadero</label><br>
             <input type="radio" value="false" name="Q[4]">
             <label>falso</label>
 
             <!--Pregunta 35-->
-            <br><p>Durante algún tiempo, cuando era joven, participé en pequeños robos</p>
+            <p>Durante algún tiempo, cuando era joven, participé en pequeños robos</p>
             <input type="radio" value="true" name="Q[5]">
-            <label>verdadero</label>
+            <label>verdadero</label><br>
             <input type="radio" value="false" name="Q[5]">
             <label>falso</label>
 
             <!--Pregunta 36-->
-            <br><p>Tengo tos la mayor parte del tiempo</p>
+            <p>Tengo tos la mayor parte del tiempo</p>
             <input type="radio" value="true" name="Q[6]">
-            <label>verdadero</label>
+            <label>verdadero</label><br>
             <input type="radio" value="false" name="Q[6]">
             <label>falso</label>
             
             <!--Pregunta 37-->
-            <br><p>A veces siento deseos de romper cosas</p>
+            <p>A veces siento deseos de romper cosas</p>
             <input type="radio" value="true" name="Q[7]">
-            <label>verdadero</label>
+            <label>verdadero</label><br>
             <input type="radio" value="false" name="Q[7]">
             <label>falso</label>
             
             <!--Pregunta 38-->
-            <br><p>He tenvalueo períodos de días, semanas o meses en los que no podía 
+            <p>He tenido períodos de días, semanas o meses en los que no podía 
                 preocuparme por las cosas, porque no tenía ánimo de nada
             </p>
             <input type="radio" value="true" name="Q[8]">
-            <label>verdadero</label>
+            <label>verdadero</label><br>
             <input type="radio" value="false" name="Q[8]">
             <label>falso</label>
             
             <!--Pregunta 39-->
-            <br><p>Mi sueño es irregular, inquieto</p>
+            <p>Mi sueño es irregular, inquieto</p>
             <input type="radio" value="true" name="Q[9]">
-            <label>verdadero</label>
+            <label>verdadero</label><br>
             <input type="radio" value="false" name="Q[9]">
             <label>falso</label>
             
             <!--Pregunta 40-->
-            <br><p>La mayor parte del tiempo parece dolerme la cabeza por todas partes</p>
+            <p>La mayor parte del tiempo parece dolerme la cabeza por todas partes</p>
             <input type="radio" value="true" name="Q[10]">
-            <label>verdadero</label>
+            <label>verdadero</label><br>
             <input type="radio" value="false" name="Q[10]">
             <label>falso</label>
             
             <!--Pregunta 41-->
-            <br><p>No digo siempre la verdad</p>
+            <p>No digo siempre la verdad</p>
             <input type="radio" value="true" name="Q[11]">
-            <label>verdadero</label>
+            <label>verdadero</label><br>
             <input type="radio" value="false" name="Q[11]">
             <label>falso</label>
             
             <!--Pregunta 42-->
-            <br><p>Si los demás no se la hubieran tomado conmigo, hubiese tenvalueo más éxito</p>
+            <p>Si los demás no se la hubieran tomado conmigo, hubiese tenido más éxito</p>
             <input type="radio" value="true" name="Q[12]">
-            <label>verdadero</label>
+            <label>verdadero</label><br>
             <input type="radio" value="false" name="Q[12]">
             <label>falso</label>
             
             <!--Pregunta 43-->
-            <br><p>Mis razonamientos son ahora mejores que nunca</p>
+            <p>Mis razonamientos son ahora mejores que nunca</p>
             <input type="radio" value="true" name="Q[13]">
-            <label>verdadero</label>
+            <label>verdadero</label><br>
             <input type="radio" value="false" name="Q[13]">
             <label>falso</label>
             
             <!--Pregunta 44-->
-            <br><p>Una vez a la semana o más a menudo, siento calor por todo el cuerpo sin 
+            <p>Una vez a la semana o más a menudo, siento calor por todo el cuerpo sin 
                 causa aparente
             </p>
             <input type="radio" value="true" name="Q[14]">
-            <label>verdadero</label>
+            <label>verdadero</label><br>
             <input type="radio" value="false" name="Q[14]">
             <label>falso</label>
             
             <!--Pregunta 45-->
-            <br><p>Tengo tan buena salud física como la mayor parte de mis amigos</p>
+            <p>Tengo tan buena salud física como la mayor parte de mis amigos</p>
             <input type="radio" value="true" name="Q[15]">
-            <label>verdadero</label>
+            <label>verdadero</label><br>
             <input type="radio" value="false" name="Q[15]">
             <label>falso</label>
             
             <!--Pregunta 46-->
-            <br><p>Prefiero hacerme el desentendvalueo con amigos del colegio, o personas 
-                conocvalueas que no veo desde hace tiempo, a menos que ellos me hablen 
+            <p>Prefiero hacerme el desentendido con amigos del colegio, o personas 
+                conocidas que no veo desde hace tiempo, a menos que ellos me hablen 
                 primero
             </p>
             <input type="radio" value="true" name="Q[16]">
-            <label>verdadero</label>
+            <label>verdadero</label><br>
             <input type="radio" value="false" name="Q[16]">
             <label>falso</label>
             
             <!--Pregunta 47-->
-            <br><p>Casi nunca he sufrvalueo de doleres en el pecho o en el corazón</p>
+            <p>Casi nunca he sufrido de doleres en el pecho o en el corazón</p>
             <input type="radio" value="true" name="Q[17]">
-            <label>verdadero</label>
+            <label>verdadero</label><br>
             <input type="radio" value="false" name="Q[17]">
             <label>falso</label>
             
             <!--Pregunta 48-->
-            <br><p>En muchas ocasiones me gustaría más sentarme y soñar despierto 
+            <p>En muchas ocasiones me gustaría más sentarme y soñar despierto 
                 que hacer cualquier cosa
             </p>
             <input type="radio" value="true" name="Q[18]">
-            <label>verdadero</label>
+            <label>verdadero</label><br>
             <input type="radio" value="false" name="Q[18]">
             <label>falso</label>
             
             <!--Pregunta 49-->
-            <br><p>Soy una persona sociable</p>
+            <p>Soy una persona sociable</p>
             <input type="radio" value="true" name="Q[19]">
-            <label>verdadero</label>
+            <label>verdadero</label><br>
             <input type="radio" value="false" name="Q[19]">
             <label>falso</label>
             
             <!--Pregunta 50-->
-            <br><p>A menudo he recibvalueo órdenes de alguien que no sabía tanto como yo</p>
+            <p>A menudo he recibido órdenes de alguien que no sabía tanto como yo</p>
             <input type="radio" value="true" name="Q[20]">
-            <label>verdadero</label>
+            <label>verdadero</label><br>
             <input type="radio" value="false" name="Q[20]">
             <label>falso</label>
             
             <!--Pregunta 51-->
-            <br><p>No leo diariamente todos los artículos principales del diario</p>
+            <p>No leo diariamente todos los artículos principales del diario</p>
             <input type="radio" value="true" name="Q[21]">
-            <label>verdadero</label>
+            <label>verdadero</label><br>
             <input type="radio" value="false" name="Q[21]">
             <label>falso</label>
 
             <!--Pregunta 52-->
-            <br><p>No he llevado un tipo de vvaluea adecuado y normal</p>
+            <p>No he llevado un tipo de vida adecuado y normal</p>
             <input type="radio" value="true" name="Q[22]">
-            <label>verdadero</label>
+            <label>verdadero</label><br>
             <input type="radio" value="false" name="Q[22]">
             <label>falso</label>
             
             <!--Pregunta 53-->
-            <br><p>A menudo algunas partes de mi cuerpo tienen sensaciones de hormigueo, 
-                quemazón, picazón o bien de quedarse "dormvalueas"
+            <p>A menudo algunas partes de mi cuerpo tienen sensaciones de hormigueo, 
+                quemazón, picazón o bien de quedarse "dormidas"
             </p>
             <input type="radio" value="true" name="Q[23]">
-            <label>verdadero</label>
+            <label>verdadero</label><br>
             <input type="radio" value="false" name="Q[23]">
             <label>falso</label>
             
             <!--Pregunta 54-->
-            <br><p>A mi familia no le gusta el trabajo que he elegvalueo (o el trabajo que 
+            <p>A mi familia no le gusta el trabajo que he elegido (o el trabajo que 
                 pienso elegir para mi futuro)</p>
             <input type="radio" value="true" name="Q[24]">
-            <label>verdadero</label>
+            <label>verdadero</label><br>
             <input type="radio" value="false" name="Q[24]">
             <label>falso</label>
             
             <!--Pregunta 55-->
-            <br><p>Algunas veces persisto en una cosa o tema hasta que los demás pierden 
+            <p>Algunas veces persisto en una cosa o tema hasta que los demás pierden 
                 la pasciencia conmigo
             </p>
             <input type="radio" value="true" name="Q[25]">
-            <label>verdadero</label>
+            <label>verdadero</label><br>
             <input type="radio" value="false" name="Q[25]">
             <label>falso</label>
             
             <!--Pregunta 56-->
-            <br><p>Me gustaría ser tan feliz como parecen los otros</p>
+            <p>Me gustaría ser tan feliz como parecen los otros</p>
             <input type="radio" value="true" name="Q[26]">
-            <label>verdadero</label>
+            <label>verdadero</label><br>
             <input type="radio" value="false" name="Q[26]">
             <label>falso</label>
             
             <!--Pregunta 57-->
-            <br><p>Muy raras veces siento dolor en la nuca</p>
+            <p>Muy raras veces siento dolor en la nuca</p>
             <input type="radio" value="true" name="Q[27]">
-            <label>verdadero</label>
+            <label>verdadero</label><br>
             <input type="radio" value="false" name="Q[27]">
             <label>falso</label>
             
             <!--Pregunta 58-->
-            <br><p>Pienso que una gran mayoría de la gente exagera sus desgracias para 
+            <p>Pienso que una gran mayoría de la gente exagera sus desgracias para 
                 lograr la simpatía y ayuda de los demás
             </p>
             <input type="radio" value="true" name="Q[28]">
-            <label>verdadero</label>
+            <label>verdadero</label><br>
             <input type="radio" value="false" name="Q[28]">
             <label>falso</label>
             
             <!--Pregunta 59-->
-            <br><p>Tengo molestias en la boca del estómago casi a diario</p>
+            <p>Tengo molestias en la boca del estómago casi a diario</p>
             <input type="radio" value="true" name="Q[29]">
-            <label>verdadero</label>
+            <label>verdadero</label><br>
             <input type="radio" value="false" name="Q[29]">
             <label>falso</label>
             
             <!--Pregunta 60-->
-            <br><p>Cuando estoy con gente escucho cosas entrañas</p>
+            <p>Cuando estoy con gente escucho cosas entrañas</p>
             <input type="radio" value="true" name="Q[30]">
-            <label>verdadero</label>
+            <label>verdadero</label><br>
             <input type="radio" value="false" name="Q[30]">
             <label>falso</label>
-            <br>
-            <input type="submit" value="Siguiente">
+
+
+
+
+
+        
+ 
+
+
+
+      
+         
+      
+          
+          <div>
+            <button type="submit" id="boton" class="submit-button">
+              <strong> Finalizar </strong>
+            </button>
+          </div>
+      
         </form>
-    </body>
+      </main>
+      
+      <footer>
+        <h3> <strong> Presione el siguiente botón para continuar </strong></h3>
+        Hecho por estudiantes del ITT
+      </footer>
+
+
+   </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
+       
