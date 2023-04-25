60ec957f2a7186c3ae9e80092e25d29e7bef41a5
Jace021099 <84101530+Jace021099@users.noreply.github.com>
2023-04-25 13:05:54 -0700

    Nuevas carpetas

---
+++ MMPI-2/Forms/MMPI-2/Form_194.php
@@ -0,0 +1,115 @@
+<?php
+session_start();
+$c = $_SESSION["counter"];
+$p = $_SESSION["pointer"];
+$temp = $_SESSION["answer"];
+
+if($p == 192){
+	$p++;
+	$c++;
+	$get = $_POST["Q"];
+	array_push($temp, $get);
+	$_SESSION["pointer"] = $p;
+	$_SESSION["counter"] = $c;
+	$_SESSION["answer"] = $temp;
+}else{	
+	if($c == 192){
+		$get = $_POST["Q"];
+		$temp[192] = $get;
+		$_SESSION["answer"] = $temp;
+		$c++;
+	}else{
+		$c--;
+	}
+	$_SESSION["counter"] = $c;
+}
+
+?>
+
+<!doctype html>
+<html lang="es">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="author" content="ITT residencia">
+	<link rel="icon" href="media/ico.ico">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<title>Pregunta #194</title>
+	<style>
+		.btn-group.respuestas {
+			margin: 0 auto;
+			display: block;
+			width: 100%;
+			text-align: center;
+		}
+		
+		.btn-group.respuestas .btn {
+			min-width: 100px;
+		}
+		.banterior:hover{
+			cursor:pointer;
+		}
+	</style>
+</head>
+
+<body>
+	<div class="container">
+		<header class="blog-header py-3">
+			<div class="row flex-nowrap justify-content-between align-items-center">
+				<div class="col-4 pt-1">
+					
+				</div>
+				<div class="col-4 text-center">
+					<img src="media/logo-big.png" class="img-fluid" style="max-height: 150px;">
+				</div>
+				<div class="col-4 d-flex justify-content-end align-items-center">
+					<a href="registro.php" id="exit_Button" onclick="return exit_Form()" class="btn btn-danger"><i class="fa fa-power-off"></i> </a>
+				</div>
+			</div>
+		</header>
+		<div class="card">
+			<div class="card-header text-center">
+				<h4>
+					
+					<span class="badge badge-success">
+						Pregunta #194
+					</span>
+				</h4>
+			</div>
+			<div class="card-body">
+
+            <form method="post" action="Form_195.php">
+
+			       
+					<!--Pregunta 194-->
+					<p>Nunca he tenido erupciones en la piel que me hayan preocupado</p>
+					<input type="radio" value="true" name="Q" onclick="enable()">
+					<label>verdadero</label><br>
+					<input type="radio" value="false" name="Q" onclick="enable()">
+					<label>falso</label>
+					<hr>
+
+			     
+            
+        	  
+					<br><br>
+					  <div class="text-center">
+					    <button type="button" id="boton_atras" onClick="history.back();" class="btn btn-success">Anterior</button>
+						<button type="submit" id="boton" disabled class="btn btn-success">Siguiente</button>
+					  </div>
+
+				<br>
+			</div>
+			<div class="card-footer text-muted text-center">
+				Lea con atención cada pregunta y responda <strong>Verdadero</strong> o <strong>Falso</strong> según corresponda.<br><br> Por favor, responda las preguntas con total honestidad.
+			</div>
+		</div>
+
+	</div>
+   
+</body>
+<script type="text/javascript" src="js/function.js"></script>
+</html>
