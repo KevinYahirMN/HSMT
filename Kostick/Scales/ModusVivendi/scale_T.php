f6c579d7bd3e4744903cc62417d7af5bdcb14bbb
Jace021099 <84101530+Jace021099@users.noreply.github.com>
2023-04-25 20:26:52 -0700

    Estructura del proyecto
    Se ordenaron las carpetas y archivos

---
+++ Kostick/Scales/ModusVivendi/scale_T.php
@@ -0,0 +1,18 @@
+<?php
+class scale_T implements Iscales{
+    function scale($answer){
+        $scaleCount = new scaleCount();
+        $c = 0;
+        $c += $scaleCount->calif_A($answer[33]);
+        $c += $scaleCount->calif_A($answer[43]);
+        $c += $scaleCount->calif_A($answer[53]);
+        $c += $scaleCount->calif_A($answer[63]);
+        $c += $scaleCount->calif_A($answer[73]);
+        $c += $scaleCount->calif_A($answer[83]);
+        $c += $scaleCount->calif_B($answer[82]);
+        $c += $scaleCount->calif_B($answer[71]);
+        $c += $scaleCount->calif_B($answer[60]);
+        return $c;
+    }
+}
+?>
---
+++ Kostick/Scales/ModusVivendi/scale_V.php
@@ -0,0 +1,18 @@
+<?php
+class scale_V implements Iscales{
+    function scale($answer){
+        $scaleCount = new scaleCount();
+        $c = 0;
+        $c += $scaleCount->calif_A($answer[44]);
+        $c += $scaleCount->calif_A($answer[54]);
+        $c += $scaleCount->calif_A($answer[64]);
+        $c += $scaleCount->calif_A($answer[74]);
+        $c += $scaleCount->calif_A($answer[84]);
+        $c += $scaleCount->calif_B($answer[83]);
+        $c += $scaleCount->calif_B($answer[72]);
+        $c += $scaleCount->calif_B($answer[61]);
+        $c += $scaleCount->calif_B($answer[50]);
+        return $c;
+    }
+}
+?>
