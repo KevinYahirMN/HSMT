912eda6c3cb6bf39f6af07aaf94462990892a712
KevinYahirMN <47574080+KevinYahirMN@users.noreply.github.com>
2023-04-26 10:24:17 -0700

    suggestions

---
+++ Kostick/Suggestions/AdaptabiltyAtWork/suggestions_D.php
@@ -0,0 +1,23 @@
+<?php
+class Suggestions_D implements ISuggestions{
+    function highPositive(){
+        $text = "";
+        return $text;
+    }
+
+    function highNegative(){
+        $text = "";
+        return $text;
+    }
+
+    function lowPositive(){
+        $text = "";
+        return $text;
+    }
+    
+    function lowNegative(){
+        $text = "";
+        return $text;
+    }
+}
+?>
---
+++ Kostick/Suggestions/Subordination/suggestions_W.php
@@ -0,0 +1,23 @@
+<?php
+class Suggestions_W implements ISuggestions{
+    function highPositive(){
+        $text = "";
+        return $text;
+    }
+
+    function highNegative(){
+        $text = "";
+        return $text;
+    }
+
+    function lowPositive(){
+        $text = "";
+        return $text;
+    }
+    
+    function lowNegative(){
+        $text = "";
+        return $text;
+    }
+}
+?>
