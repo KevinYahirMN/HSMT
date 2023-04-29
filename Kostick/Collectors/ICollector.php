3a9bd447c8a86ba7bb62578e6a14162b7045fb12
Jace021099 <84101530+Jace021099@users.noreply.github.com>
2023-04-28 19:11:11 -0700

    Comentarios necesarios

--- Kostick/Collectors/ICollector.php
+++ Kostick/Collectors/ICollector.php
@@ -1,6 +1,10 @@
 <?php
 
 interface ICollector{
+
+    /*Recibe un arreglo para realizar el conteo
+    o seleccionar las sujerencias de interpretación
+    a partir del conteo por cada categoría*/
     function DAA($array);
 
     function DV($array);
