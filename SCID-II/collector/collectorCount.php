02a9e1e945ec869c37f132861360aa80b80d3f80
Jace021099 <84101530+Jace021099@users.noreply.github.com>
2023-05-02 13:20:27 -0700

    Update collectorCount.php

--- SCID-II/collector/collectorCount.php
+++ SCID-II/collector/collectorCount.php
@@ -1,5 +1,16 @@
 <?php
 include_once('../scales/scale_evitation.php');
+include_once('../scales/scale_dependent.php');
+include_once('../scales/scale_obsessive.php');
+include_once('../scales/scale_passive_aggressive.php');
+include_once('../scales/scale_passive-dependent.php');
+include_once('../scales/scale_paranoid.php');
+include_once('../scales/scale_schizotypal.php');
+include_once('../scales/scale_schizoid.php');
+include_once('../scales/scale_histrionic.php');
+include_once('../scales/scale_narcissistic.php');
+include_once('../scales/scale_limit.php');
+include_once('../scales/scale_antisocial.php');
 
 class CollectorCount{
     function evitation($answer){
@@ -9,68 +20,68 @@ class CollectorCount{
     }
 
     function dependent($answer){
-        $ = new ;
-        $c = $->scale($answer);
+        $dep = new scale_dependent;
+        $c = $dep->scale($answer);
         return $c;
     }
 
     function obsessive($answer){
-        $ = new ;
-        $c = ->scale($answer);
+        $obs = new scale_obsessive;
+        $c = $obs->scale($answer);
         return $c;
     }
 
     function passive_agressive($answer){
-        $ = new ;
-        $c = $->scale($answer);
+        $pa = new scale_passive_aggressive;
+        $c = $pa->scale($answer);
         return $c;
     }
 
     function passive_dependent($answer){
-        $ = new ;
-        $c = ->scale($answer);
+        $pd = new scale_passive_dependent;
+        $c = $pd->scale($answer);
         return $c;
     }
 
     function paranoid($answer){
-        $ = new ;
-        $c = $->scale($answer);
+        $p = new scale_paranoid;
+        $c = $p->scale($answer);
         return $c;
     }
 
     function schizotypal($answer){
-        $ = new ;
-        $c = ->scale($answer);
+        $sch = new scale_schizotypal;
+        $c = $sch->scale($answer);
         return $c;
     }
 
     function schizoid($answer){
-        $ = new ;
-        $c = $->scale($answer);
+        $sch = new scale_schizoid;
+        $c = $sch->scale($answer);
         return $c;
     }
 
     function histrionic($answer){
-        $ = new ;
-        $c = ->scale($answer);
+        $h = new scale_histrionic;
+        $c = $h->scale($answer);
         return $c;
     }
 
     function narcissistic($answer){
-        $ = new ;
-        $c = $->scale($answer);
+        $n = new scale_narcissistic;
+        $c = $n->scale($answer);
         return $c;
     }
 
     function limit($answer){
-        $ = new ;
-        $c = ->scale($answer);
+        $l = new scale_limit;
+        $c = $l->scale($answer);
         return $c;
     }
 
     function antisocial($answer){
-        $ = new ;
-        $c = $->scale($answer);
+        $ant = new scale_antisocial;
+        $c = $ant->scale($answer);
         return $c;
     }
 }
