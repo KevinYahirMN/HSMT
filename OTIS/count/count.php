584f9338d26645b3378a43a758b05f468fd6b81e
Jace021099 <84101530+Jace021099@users.noreply.github.com>
2023-05-08 14:14:09 -0700

    Carpetas

---
+++ OTIS/count/count.php
@@ -0,0 +1,165 @@
+<?php
+class scaleCount{
+    
+    //sumatoria de respuesta a
+    function calif_A($answer){
+        if($answer == 'A'){
+            return 1;
+        }
+    }
+    
+    //Sumatoria de respuesta b
+    function calif_B($answer){
+        if($answer == 'B'){
+            return 1;
+        }
+    }
+
+    //Sumatoria de respuesta c
+    function calif_C($answer){
+        if($answer == 'C'){
+            return 1;
+        }
+    }
+
+    //Sumatoria de respuesta d
+    function calif_D($answer){
+        if($answer == 'D'){
+            return 1;
+        }
+    }
+
+    //Sumatoria de respuesta e
+    function calif_E($answer){
+        if($answer == 'E'){
+            return 1;
+        }
+    }
+
+    //Sumatoria de respuesta f
+    function calif_F($answer){
+        if($answer == 'F'){
+            return 1;
+        }
+    }
+    
+    //Sumatoria de respuesta g
+    function calif_G($answer){
+        if($answer == 'G'){
+            return 1;
+        }
+    }
+
+    //Sumatoria de respuesta h
+    function calif_H($answer){
+        if($answer == 'H'){
+            return 1;
+        }
+    }
+
+    //Sumatoria de respuesta i
+    function calif_I($answer){
+        if($answer == 'I'){
+            return 1;
+        }
+    }
+    
+    //Sumatoria de respuesta j
+    function calif_J($answer){
+        if($answer == 'J'){
+            return 1;
+        }
+    }
+
+    //Sumatoria de respuesta m
+    function calif_M($answer){
+        if($answer == 'M'){
+            return 1;
+        }
+    }
+
+    //Sumatoria de respuesta o
+    function calif_O($answer){
+        if($answer == 'O'){
+            return 1;
+        }
+    }
+
+    //Sumatoria de respuesta q 
+    function calif_Q($answer){
+        if($answer == 'Q'){
+            return 1;
+        }
+    }
+
+    //Sumatoria de respuesta t
+    function calif_T($answer){
+        if($answer == 'T'){
+            return 1;
+        }
+    }
+
+    //Sumatoria de respuesta 2
+    function calif_Two($answer){
+        if($answer == 2){
+            return 1;
+        }
+    }
+
+    //Sumatoria de respuesta 3
+    function calif_three($answer){
+        if($answer == 3){
+            return 1;
+        }
+    }
+
+    //Sumatoria de respuesta 4
+    function calif_four($answer){
+        if($answer == 4){
+            return 1;
+        }
+    }
+
+    //Sumatoria de respuesta 7
+    function calif_Seven($answer){
+        if($answer == 7){
+            return 1;
+        }
+    }
+
+    //Sumatoria de respuesta 8
+    function calif_Eight($answer){
+        if($answer == 8){
+            return 1;
+        }
+    }
+
+    //Sumatoria de respuesta 10
+    function calif_Ten($answer){
+        if($answer == 10){
+            return 1;
+        }
+    }
+
+    //Sumatoria de respuesta 13
+    function calif_Thirteen($answer){
+        if($answer == 13){
+            return 1;
+        }
+    }
+
+    //Sumatoria de respuesta 16
+    function calif_Sixteen($answer){
+        if($answer == 16){
+            return 1;
+        }
+    }
+
+    //Sumatoria de respuesta 17
+    function calif_Seventeen($answer){
+        if($answer == 17){
+            return 1;
+        }
+    }
+}
+?>
