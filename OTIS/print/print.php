fe4853ea530925071b7d0fed8f4d22460e67c4e9
KevinYahirMN <47574080+KevinYahirMN@users.noreply.github.com>
2023-05-08 14:15:23 -0700

    pruebas

--- OTIS/print/print.php
+++ OTIS/print/print.php
@@ -1,12 +1,11 @@
 <?php
-include("../collector/CollectorCount.php");
+/*include("../collector/CollectorCount.php");
 session_start();
 $temp = $_SESSION["answer"];
 $get = $_POST["Q"];
 array_push($temp, $get);
 $_SESSION["answer"] = $temp;
-
-
+*/
 ?>
 
 <html>
@@ -21,84 +20,35 @@ $_SESSION["answer"] = $temp;
 
         <thead>
           <tr>
-            <th>PERSONALIDAD</th>
-            <th>%</th>
+            <th>Reactivo</th>
+            <th>Respuesta</th>
+            <th>Reactivo</th>
+            <th>Respuesta</th>
+            <th>Reactivo</th>
+            <th>Respuesta</th>
+            <th>Reactivo</th>
+            <th>Respuesta</th>
+            <th>Reactivo</th>
+            <th>Respuesta</th>
           </tr>
         </thead>
         <tbody>
         <tr>
-    <td>Evitación</td>
-    <td><?php echo $evitation;?></td>
-  </tr>
-  <tr>
-    <td>Dependiente</td>
-    <td><?php echo $dependent;?></td>
-  </tr>
-  <tr>
-    <td>Obsesivo</td>
-    <td><?php echo $obsessive;?></td>
-  </tr>
-  <tr>
-    <td>Pasivo-Agresivo</td>
-    <td><?php echo $passive_agressive;?></td>
-  </tr>
-  <tr>
-    <td>Pasivo-Dependiente</td>
-    <td><?php echo $passive_dependent;?></td>
-  </tr>
-  <tr>
-    <td>Paranoide</td>
-    <td><?php echo $paranoid;?></td>
-  </tr>
-  <tr>
-    <td>Esquizotipico</td>
-    <td><?php echo $schizotypal;?></td>
-  </tr>
-  <tr>
-    <td>Esquizoide</td>
-    <td><?php echo $schizoid;?></td>
-  </tr>
-  <tr>
-    <td>Histriónico</td>
-    <td><?php echo $histrionic;?></td>
-  </tr>
-  <tr>
-    <td>Narcisista</td>
-    <td><?php echo $narcissistic;?></td>
-  </tr>
-  <tr>
-    <td>Limite</td>
-    <td><?php echo $limit;?></td>
-  </tr>
-  <tr>
-    <td>Antisocial</td>
-    <td><?php echo $antisocial;?></td>
-  </tr>
+    <td>1</td>
+    <td></td>
+    <td>15</td>
+    <td></td>
+    <td>31</td>
+    <td></td>
+    <td>46</td>
+    <td></td>
+    <td>612</td>
+    <td></td>
+       </tr>
+
         </tbody>
       </table>
 </body>
-</html>
-					  <br><br>
-					  <div class="text-center">
-						<button style="background-color: #4CAF50; 
-            color: white; 
-            padding: 10px 20px; 
-            border: none;
-            font-size: 16px;
-            padding: 8px 20px;
-            border-radius: 4px; 
-            cursor: pointer;"
-            onclick="window.print();" id="print" class="print_Button">Imprimir</button>
-					  </div>
-				<br>
-			</div>
-			<div class="card-footer text-muted text-center">
-			<Strong>Nota: </Strong>Este reporte no debe ser tomado textualmente pues presenta solo inferencias generales acerca del significado de la elevación de las puntuaciones que deben considerarse en cada caso de acuerdo con la historia y antecedentes del sujeto.
-			</div>
-		</div>
-	</div>
-</body>
-<script type="text/javascript" src="../forms/js/function.js"></script>
 </html>
 
 <?php
