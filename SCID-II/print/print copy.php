c8c9960de39581376f238b803d114db24f061a91
KevinYahirMN <47574080+KevinYahirMN@users.noreply.github.com>
2023-05-30 12:24:32 -0700

    pruebas

--- SCID-II/print/print copy.php
+++ SCID-II/print/print copy.php
@@ -1,27 +1,26 @@
 <?php
-include("../collector/collectorCount.php");
-include("../collector/collectorDx.php");
-include("../collector/collectorIQ.php");
-include("../collector/collectorRange.php");
-
+include("../collector/CollectorCount.php");
 $temp = array();
-for($i = 0; $i < 75; $i++){
+for($i = 0; $i < 120; $i++){
   if($i%2 == 0){
     array_push($temp, 'A');
   }else{
     array_push($temp, 'B');
   }
 }
-
-$CollectorCount = new collectorCount();
-$CollectorDx = new collectorDx();
-$CollectorIQ = new collectorIQ();
-$CollectorRange = new collectorRange();
-
-$collect = $CollectorCount->collect($temp);
-$IQ = $CollectorIQ->collect($collect);
-$Range = $CollectorRange->collect($IQ);
-$Dx = $CollectorDx->collect($IQ);
+$collectorCount = new CollectorCount();
+$evitation = $collectorCount->evitation($temp);
+$dependent = $collectorCount->dependent($temp);
+$obsessive = $collectorCount->obsessive($temp);
+$passive_agressive = $collectorCount->passive_agressive($temp);
+$passive_dependent = $collectorCount->passive_dependent($temp);
+$paranoid = $collectorCount->paranoid($temp);
+$schizotypal = $collectorCount->schizotypal($temp);
+$schizoid = $collectorCount->schizoid($temp);
+$histrionic = $collectorCount->histrionic($temp);
+$narcissistic = $collectorCount->narcissistic($temp);
+$limit = $collectorCount->limit($temp);
+$antisocial = $collectorCount->antisocial($temp);
 ?>
 
 <!doctype html>
@@ -35,8 +34,8 @@ $Dx = $CollectorDx->collect($IQ);
 	<link rel="stylesheet" href="../forms/css/font-awesome.min.css">
 	<link rel="stylesheet" href="../forms/css/style.css">
   <link rel="stylesheet" href="Print.css">
-  <link rel="stylesheet" href="table.css">
-
+  <link rel="stylesheet" href="Table.css">
+  <link href='Table.css' rel='stylesheet' type='text/css'>
 	<title>Resultados</title>
 	<style>
 		.btn-group.respuestas {
@@ -173,225 +172,71 @@ $Dx = $CollectorDx->collect($IQ);
         </div>
 	</center>
 
-
+<html>
+<head>
+    <link rel="stylesheet" href="table.css">
+</head>
 <body>
 <br>
 <hr>
     <table class="content-table">
     <div class="col-md-12 text-center text-success"><h3>RESULTADOS</h3></div>
 
-    <thead>
-          <tr>
-            <th>Reactivo</th>
-            <th>Respuesta</th>
-            <th>Reactivo</th>
-            <th>Respuesta</th>
-            <th>Reactivo</th>
-            <th>Respuesta</th>
-            <th>Reactivo</th>
-            <th>Respuesta</th>
-            <th>Reactivo</th>
-            <th>Respuesta</th>
-          </tr>
-        </thead>
-        <tbody>
-        <tr>
-    <td>1</td>
-    <td><?php echo $temp[0];?></td>
-    <td>16</td>
-    <td><?php echo $temp[15];?></td>
-    <td>31</td>
-    <td><?php echo $temp[30];?></td>
-    <td>46</td>
-    <td><?php echo $temp[35];?></td>
-    <td>61</td>
-    <td><?php echo $temp[60];?></td>
-       </tr>
-       <tr>
-    <td>2</td>
-    <td><?php echo $temp[1];?></td>
-    <td>17</td>
-    <td><?php echo $temp[16];?></td>
-    <td>32</td>
-    <td><?php echo $temp[31];?></td>
-    <td>47</td>
-    <td><?php echo $temp[46];?></td>
-    <td>62</td>
-    <td><?php echo $temp[61];?></td>
-       </tr>
-       <tr>
-    <td>3</td>
-    <td><?php echo $temp[2];?></td>
-    <td>18</td>
-    <td><?php echo $temp[17];?></td>
-    <td>33</td>
-    <td><?php echo $temp[32];?></td>
-    <td>48</td>
-    <td><?php echo $temp[47];?></td>
-    <td>63</td>
-    <td><?php echo $temp[64];?></td>
-       </tr>
-       <tr>
-    <td>4</td>
-    <td><?php echo $temp[3];?></td>
-    <td>19</td>
-    <td><?php echo $temp[18];?></td>
-    <td>34</td>
-    <td><?php echo $temp[33];?></td>
-    <td>49</td>
-    <td><?php echo $temp[48];?></td>
-    <td>64</td>
-    <td><?php echo $temp[65];?></td>
-       </tr>
-       <tr>
-    <td>5</td>
-    <td><?php echo $temp[4];?></td>
-    <td>20</td>
-    <td><?php echo $temp[19];?></td>
-    <td>35</td>
-    <td><?php echo $temp[34];?></td>
-    <td>50</td>
-    <td><?php echo $temp[49];?></td>
-    <td>65</td>
-    <td><?php echo $temp[64];?></td>
-       </tr>
-       <tr>
-    <td>6</td>
-    <td><?php echo $temp[5];?></td>
-    <td>21</td>
-    <td><?php echo $temp[20];?></td>
-    <td>36</td>
-    <td><?php echo $temp[35];?></td>
-    <td>51</td>
-    <td><?php echo $temp[50];?></td>
-    <td>66</td>
-    <td><?php echo $temp[65];?></td>
-       </tr>
-       <tr>
-    <td>7</td>
-    <td><?php echo $temp[6];?></td>
-    <td>22</td>
-    <td><?php echo $temp[21];?></td>
-    <td>37</td>
-    <td><?php echo $temp[36];?></td>
-    <td>52</td>
-    <td><?php echo $temp[51];?></td>
-    <td>67</td>
-    <td><?php echo $temp[66];?></td>
-       </tr>
-       <tr>
-    <td>8</td>
-    <td><?php echo $temp[7];?></td>
-    <td>23</td>
-    <td><?php echo $temp[22];?></td>
-    <td>38</td>
-    <td><?php echo $temp[37];?></td>
-    <td>53</td>
-    <td><?php echo $temp[52];?></td>
-    <td>68</td>
-    <td><?php echo $temp[67];?></td>
-       </tr>
-       <tr>
-    <td>9</td>
-    <td><?php echo $temp[8];?></td>
-    <td>24</td>
-    <td><?php echo $temp[23];?></td>
-    <td>39</td>
-    <td><?php echo $temp[38];?></td>
-    <td>54</td>
-    <td><?php echo $temp[53];?></td>
-    <td>69</td>
-    <td><?php echo $temp[68];?></td>
-    <tr>
-    <td>10</td>
-    <td><?php echo $temp[9];?></td>
-    <td>25</td>
-    <td><?php echo $temp[24];?></td>
-    <td>40</td>
-    <td><?php echo $temp[39];?></td>
-    <td>55</td>
-    <td><?php echo $temp[54];?></td>
-    <td>70</td>
-    <td><?php echo $temp[69];?></td>
-    <tr>
-    <td>11</td>
-    <td><?php echo $temp[10];?></td>
-    <td>26</td>
-    <td><?php echo $temp[25];?></td>
-    <td>41</td>
-    <td><?php echo $temp[40];?></td>
-    <td>56</td>
-    <td><?php echo $temp[55];?></td>
-    <td>71</td>
-    <td><?php echo $temp[70];?></td>
-       </tr>
-       <tr>
-    <td>12</td>
-    <td><?php echo $temp[11];?></td>
-    <td>27</td>
-    <td><?php echo $temp[26];?></td>
-    <td>42</td>
-    <td><?php echo $temp[41];?></td>
-    <td>57</td>
-    <td><?php echo $temp[58];?></td>
-    <td>72</td>
-    <td><?php echo $temp[71];?></td>
-       </tr>
-       <tr>
-    <td>13</td>
-    <td><?php echo $temp[12];?></td>
-    <td>28</td>
-    <td><?php echo $temp[27];?></td>
-    <td>43</td>
-    <td><?php echo $temp[42];?></td>
-    <td>58</td>
-    <td><?php echo $temp[57];?></td>
-    <td>73</td>
-    <td><?php echo $temp[72];?></td>
-       </tr>
-       <tr>
-    <td>14</td>
-    <td><?php echo $temp[13];?></td>
-    <td>29</td>
-    <td><?php echo $temp[28];?></td>
-    <td>44</td>
-    <td><?php echo $temp[43];?></td>
-    <td>59</td>
-    <td><?php echo $temp[58];?></td>
-    <td>74</td>
-    <td><?php echo $temp[73];?></td>
-       </tr>
-       <tr>
-    <td>15</td>
-    <td><?php echo $temp[14];?></td>
-    <td>30</td>
-    <td><?php echo $temp[29];?></td>
-    <td>45</td>
-    <td><?php echo $temp[44];?></td>
-    <td>60</td>
-    <td><?php echo $temp[59];?></td>
-    <td>75</td>
-    <td><?php echo $temp[74];?></td>
-       </tr>
-        </tbody>
-      </table>
-      <br>
-      <table class="content-table">
         <thead>
           <tr>
-            <th>Puntuación Directa (PD)</th>
-            <th>Coeficiente Intelectual</th>
-            <th>Rango</th>
-            <th>Dx</th>
+            <th>PERSONALIDAD</th>
+            <th>%</th>
           </tr>
         </thead>
         <tbody>
         <tr>
-     <td><?php echo $collect;?></td>
-     <td><?php echo $IQ;?></td>
-     <td><?php echo $Range;?></td>
-     <td><?php echo $Dx;?></td>
-       </tr>
+    <td>Evitación</td>
+    <td><?php echo $evitation;?></td>
+  </tr>
+  <tr>
+    <td>Dependiente</td>
+    <td><?php echo $dependent;?></td>
+  </tr>
+  <tr>
+    <td>Obsesivo</td>
+    <td><?php echo $obsessive;?></td>
+  </tr>
+  <tr>
+    <td>Pasivo-Agresivo</td>
+    <td><?php echo $passive_agressive;?></td>
+  </tr>
+  <tr>
+    <td>Pasivo-Dependiente</td>
+    <td><?php echo $passive_dependent;?></td>
+  </tr>
+  <tr>
+    <td>Paranoide</td>
+    <td><?php echo $paranoid;?></td>
+  </tr>
+  <tr>
+    <td>Esquizotipico</td>
+    <td><?php echo $schizotypal;?></td>
+  </tr>
+  <tr>
+    <td>Esquizoide</td>
+    <td><?php echo $schizoid;?></td>
+  </tr>
+  <tr>
+    <td>Histriónico</td>
+    <td><?php echo $histrionic;?></td>
+  </tr>
+  <tr>
+    <td>Narcisista</td>
+    <td><?php echo $narcissistic;?></td>
+  </tr>
+  <tr>
+    <td>Limite</td>
+    <td><?php echo $limit;?></td>
+  </tr>
+  <tr>
+    <td>Antisocial</td>
+    <td><?php echo $antisocial;?></td>
+  </tr>
         </tbody>
       </table>
 </body>
@@ -418,6 +263,7 @@ $Dx = $CollectorDx->collect($IQ);
 </body>
 <script type="text/javascript" src="../forms/js/function.js"></script>
 </html>
+
 <?php
 $_SESSION["answer"] = null;
 $_SESSION["answer"] = array();
